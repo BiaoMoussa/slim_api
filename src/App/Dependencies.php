be38ead8c98395f6f1c448beb7a07c82c3203c74
Moubarak Laouali <moubaraklaouali2014@gmail.com>
2024-07-02 01:00:33 +0100

    time zone base des données

--- src/App/Dependencies.php
+++ src/App/Dependencies.php
@@ -13,11 +13,14 @@ $dsn = sprintf(
     $database['name'],
     $database['port']
 );
-$pdo = new PDO($dsn, $database['user'], $database['pass']);
-$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-$pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-$pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
-$pdo->setAttribute(PDO::MYSQL_ATTR_INIT_COMMAND,"SET time_zone = 'Africa/Niamey'");
+$options = array(
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES=>false,
+    PDO::MYSQL_ATTR_INIT_COMMAND => "SET time_zone = 'Africa/Niamey'"
+);
+$pdo = new PDO($dsn, $database['user'], $database['pass'], $options);
+
 $GLOBALS["pdo"] = $pdo;
 $GLOBALS["errorHandler"]=static fn (): ApiError => new ApiError();
 $container = $GLOBALS["errorHandler"];
