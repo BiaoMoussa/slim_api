691815b70c9e62f41e684feed7740e9540f900ed
Moubarak Laouali <moubaraklaouali2014@gmail.com>
2024-07-02 11:46:29 +0100

    Correction bug time zone

--- src/App/Dependencies.php
+++ src/App/Dependencies.php
@@ -17,7 +17,7 @@ $options = array(
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
     PDO::ATTR_EMULATE_PREPARES=>false,
-    PDO::MYSQL_ATTR_INIT_COMMAND => "SET time_zone = 'Africa/Niamey'"
+    //PDO::MYSQL_ATTR_INIT_COMMAND => "SET time_zone = 'Africa/Niamey'"
 );
 $pdo = new PDO($dsn, $database['user'], $database['pass'], $options);
 
